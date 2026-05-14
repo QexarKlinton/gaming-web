10b1ab66ef884dec69b615a934ce38bbe83aa665
Cesar Medina Hernandez <cesar.medina0687@alumnos.udg.mx>
2026-05-14 16:34:35 -0600

    fix railway mysql

--- server/index.js
+++ server/index.js
@@ -16,9 +16,14 @@ app.use(express.json())
 const db = mysql.createConnection({
 
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+
+  ssl: {
+    rejectUnauthorized: false
+  }
 
 })
 
